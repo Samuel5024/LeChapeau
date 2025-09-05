ef29ac24a4af729b10b13185eefb978f0c3eae84
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-09-05 07:40:55 -0700

    Page 43 of Part two
    some issues with RPC stuff in the code and IsMine

---
+++ Le Chapeau Part 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks //change inherited class
+{
+    //here are the variables
+    [Header("Stats")]                 
+    public bool gameEnded = false;      //has the game ended?
+    public float timeToWin;             //time a player needs to hold the hat to win
+    public float invincibleDuration;    //how llong afater a player gets the hat, are they invincible
+    private float hatPickupTime;        //the time the hat was picked up by the current holder
+
+    [Header("Players")]
+    public string playerPrefabLocation; //path in resources folder to the Player prefab
+    public Transform[] spawnPoints;
+    public PlayerController[] players;   //array of al available spawn points
+    public int playerWithHat;           //id of player with the hat
+    private int playersInGame;          //number of players in the game
+
+    //instance
+    public static GameManager instance;
+
+    void Awake()
+    {
+        //instance
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() //setup the players array
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.All);
+    }
+
+    //ImInGame gets called when a player joins the game
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    //SpawPlayer gets called when all players are in the game
+    //spawns a player and initializes it
+    void SpawnPlayer()
+    {
+        //instantiate the player across the network
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints 
+            [Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        //get the player script
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+
+        //initialize the player
+        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    //the following functions take in an id or game object, then returns the corresponding player
+
+    public PlayerController GetPlayer(int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+
+    public PlayerController GetPlayer(GameObject playerObject)
+    {
+        return players.First(x => gameObject == playerObject);
+    }
+
+    //called when a player hits the hatted player - giving them the hat
+    [PunRPC]
+    public void GiveHat(int playerId, bool intitialGive)
+    {
+        //remove the hat from the currently hatted player
+        if (!initialGive)
+            GetPlayer(playerWithHat).SetHat(false);
+
+        //give the hat to the new player
+        playerWithHat = playerId;
+        GetPlayer(playerId).SetHat(true);
+        hatPickupTime = Time.time;
+    }
+
+    //is the player able to take the hat at this current time?
+    public bool CanGetHat()
+    {
+        if (Time.time > hatPickupTime + invincibleDuration)
+            return true;
+        else
+            return false;
+    }
+
+    [PunRPC]
+    void WinGame(int playerId)
+    {
+        gameEnded = true;
+        PlayerController player = GetPlayer(playerId);
+        //set the UI to show who's won
+
+        Invoke("GoBackToMenu", 3.0f);
+    }
+
+    void GoBackToMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        NetworkManager.instance.ChangeScene("Menu");
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Le Chapeau Part 2/Assets/Scripts/PlayerController.cs
+++ Le Chapeau Part 2/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
+
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector]
+    public int id;
+
+    [Header("Info")]
+    public float moveSpeed;
+    public float jumpForce;
+    public GameObject hatObject;
+
+    [HideInInspector]
+    public float curHatTime;
+
+    [Header("Components")]
+    public Rigidbody rig;
+    public Player photonPlayer;
+
+    public object photonView { get; internal set; }
+
+    //called when the player object is instantiated
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+
+        GameManager.instance.players[id - 1] = this;
+
+        //give the first player the hat
+        if (id == 1)
+            GameManager.instance.GiveHat(id, true);
+        //if this isn't our local player, disable all physics that's controlled by the user and sycned to all other clients
+        if (PhotonView.isMine)
+                rig.isKinematic = true;
+    }
+
+        // Start is called before the first frame update
+        void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(photonView.IsMine)
+        {
+            Move();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                TryJump();
+
+            //track the amount of time we're wearing the hat
+            if (hatObject.activeInHierarchy)
+            {
+                curHatTime += Time.deltaTime;
+            }
+        }
+
+        //the host will check if the player has won
+        if(PhotonNetwork.IsMasterClient)
+        {
+            if(curHatTime >= GameManager.instance.timeToWin && !GameManager.instance.gameEnded)
+            {
+                GameManager.instance.gameEnded = true;
+                GameManager.instance.photonView.RPC("WinGame", RpcTarget.All, id);
+            }
+        }
         
+    }
+
+    void Move() //checks for keyboad input and sets rigidbody's velocity
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+    }
+
+    void TryJump() //checks if we're standing on the ground and if so, add upwards force
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 0.7f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    //sets players hat active or not
+    public void SetHat(bool hasHat)
+    {
+        hatObject.SetActive(hasHat);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!photonView.IsMine)
+            return;
+
+        //did we hit another player
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            //do they have the hat?
+            if(GameManager.instance.GetPlayer(collision.gameObject).id == GameManager.instance.playerWithHat)
+            {
+                //can we get the hat?
+                if(GameManager.instance.CanGetHat())
+                {
+                    //give us the hat
+                    GameManager.instance.photonView.RPC("GiveHat", RpcTarget.All, id, false);
+                }
+            }
+        }
     }
 }
